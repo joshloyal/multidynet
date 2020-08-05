4ddaab8474796bba92aaab771c4a640a557cdeb7
Joshua Loyal <jloyal25@gmail.com>
2020-08-05 10:24:06 -0500

    added icews dataset

--- multidynet/datasets/load_icews.py
+++ multidynet/datasets/load_icews.py
@@ -1,4 +1,5 @@
 import numpy as np
+import joblib
 
 from os.path import dirname, join
 
@@ -10,18 +11,11 @@ def load_icews():
     module_path = dirname(__file__)
     file_path = join(module_path, 'raw_data')
 
-    n_nodes = 65
-    n_layers = 4
-    n_time_steps = 96
-    Y = np.zeros((n_layers, n_time_steps, n_nodes, n_nodes))
-
-    file_fmt = join('icews', 'icews_{}_{}.npy')
-    for k in range(n_layers):
-        for t in range(n_time_steps):
-            Y[k, t] = np.loadtxt(join(file_path, file_fmt.format(k+1, t+1)))
+    file_name = join(file_path, 'icews', 'numpy_data', 'icews_networks.gz')
+    Y = joblib.load(open(file_name, 'rb'))
 
     countries = np.loadtxt(
-        join(file_path, 'icews', 'icews_countries.txt'),
+        join(file_path, 'icews', 'numpy_data', 'icews_countries.txt'),
         delimiter='\n', dtype=np.unicode)
 
     return np.ascontiguousarray(Y), countries
