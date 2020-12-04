e82916a82d7ab8a7bd52b81b3f38b989d2e9b402
Joshua Loyal <jloyal25@gmail.com>
2020-12-04 15:43:06 -0600

    new dist calc

--- multidynet/multidynet.py
+++ multidynet/multidynet.py
@@ -274,18 +274,15 @@ def calculate_probabilities(X, lmbda, delta):
 
     probas = np.zeros(
         (n_layers, n_time_steps, n_nodes, n_nodes), dtype=np.float64)
-    dist = np.zeros(
-        (n_layers, n_time_steps, n_nodes, n_nodes), dtype=np.float64)
     for k in range(n_layers):
         for t in range(n_time_steps):
             deltakt = delta[k, t].reshape(-1, 1)
             eta = np.add(deltakt, deltakt.T)
             if X is not None:
-                dist[k, t] = np.dot(X[t] * lmbda[k], X[t].T)
-                eta += dist[k, t]
+                eta += np.dot(X[t] * lmbda[k], X[t].T)
             probas[k, t] = expit(eta)
 
-    return probas, dist
+    return probas
 
 
 class DynamicMultilayerNetworkLSM(object):
@@ -428,8 +425,17 @@ class DynamicMultilayerNetworkLSM(object):
         self._set_parameters(best_model)
 
         # calculate dyad-probabilities
-        self.probas_, self.dist_ = calculate_probabilities(
+        self.probas_ = calculate_probabilities(
             self.X_, self.lambda_, self.delta_)
+
+        # identifiable distances
+        n_layers, n_time_steps, n_nodes, _ = Y.shape
+        self.dist_ = np.zeros(
+            (n_layers, n_time_steps, n_nodes, n_nodes), dtype=np.float64)
+        for k in range(n_layers):
+            for t in range(n_time_steps):
+                self.dist_[k, t] = np.dot(
+                    self.Z_[t] * self.lambda_[k], self.Z_[t].T)
 
         # calculate in-sample AUC
         self.auc_ = calculate_auc(Y, self.probas_)
