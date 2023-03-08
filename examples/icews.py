76f25573226731d5e75462df3cc0bb422d65adda
Joshua Loyal <joshua.d.loyal@gmail.com>
2023-03-08 15:53:10 -0500

    removed unnecessary examples

--- examples/icews.py
+++ examples/icews.py
@@ -1,29 +1,9 @@
-import os
-
-import joblib
-import numpy as np
-
 from multidynet import DynamicMultilayerNetworkLSM
 from multidynet.datasets import load_icews
-from multidynet.model_selection import train_test_split
-from multidynet.metrics import calculate_auc
 
 
 Y, countries, layer_labels, time_labels = load_icews(dataset='large')
 
 
-model = DynamicMultilayerNetworkLSM(max_iter=50, n_features=4,
-                                    lambda_var_prior=10.,
-                                    lambda_odds_prior=1,
-                                    tol=1e-2, init_type='svt',
-                                    init_covariance_type='full',
-                                    random_state=82590)
+model = DynamicMultilayerNetworkLSM(max_iter=50, n_features=2, random_state=42)
 model.fit(Y)
-
-
-#out_dir = 'icews_results'
-#if not os.path.exists(out_dir):
-#    os.makedirs(out_dir)
-#out_file = os.path.join(out_dir, 'icews_model.pkl')
-#
-#joblib.dump(model, out_file, compress=('zlib', 6))
--- examples/primaryschool.py
+++ examples/primaryschool.py
@@ -1,25 +1,8 @@
-import os
-
-import joblib
-
 from multidynet import DynamicMultilayerNetworkLSM
 from multidynet.datasets import load_primaryschool
-from multidynet.model_selection import train_test_split
-from multidynet.metrics import calculate_auc
 
 
 Y, _, _, _, _ = load_primaryschool()
 
-model = DynamicMultilayerNetworkLSM(max_iter=50, n_features=2,
-                                    lambda_var_prior=10.,
-                                    lambda_odds_prior=1,
-                                    tol=1e-2, init_type='svt',
-                                    random_state=82590)
+model = DynamicMultilayerNetworkLSM(max_iter=50, n_features=2, random_state=42)
 model.fit(Y)
-
-#out_dir = 'school_results'
-#if not os.path.exists(out_dir):
-#    os.makedirs(out_dir)
-#out_file = os.path.join(out_dir, 'school_model.pkl')
-#
-#joblib.dump(model, out_file, compress=('zlib', 6))
